0bdb407ff115b0737290470ea3e39789571c1067
Muhammad Zidan <zidanr180@gmail.com>
2023-12-12 14:16:03 +0700

    Upload Files

---
+++ sejarah.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<link rel="icon" href="img/sipekaaja.png" type="image/png">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SiPeka | Sejarah</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:400,700&display=swap">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.22.0/font/bootstrap-icons.css">
+
+    
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="navbar" id="navbar">
+    <div class="logo">
+        <a href="#">
+            <img src="img/sipekaaja.png" alt="Logo"> 
+            Si<span>Peka</span>
+        </a>
+    </div>
+    <div class="menu">
+    <a href="index.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-speedometer2" viewBox="0 0 16 16">
+  <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
+  <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
+</svg> Dashboard</a>
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-history" viewBox="0 0 16 16">
+  <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022l-.074.997zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342l-.36.933zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483zm.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z"/>
+  <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0v1z"/>
+  <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"/>
+</svg> Sejarah</a>
+        <a href="kejadian.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-collection" viewBox="0 0 16 16">
+  <path d="M2.5 3.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm2-2a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zm1.5.5A.5.5 0 0 1 1 13V6a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13z"/>
+</svg> Kejadian</a>
+        <a href="sop.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book-fill" viewBox="0 0 16 16">
+  <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+</svg> SOP</a>
+        <a href="konatak.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+  <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
+</svg> Kontak</a>
+    </div>
+</div>
+
+<div style="margin-top: 60px;">
+   
+</div>
+
+    
+<!-- header -->
+
+
+
+<div id="apanih">
+    <br>
+<p>Sipeka | Mengenal Bendungan Pice > Sejarah Bendungan Pice
+
+ 
+
+</p>
+      <br>
+    </div>
+
+    <br>
+    
+
+<h1 class="hop">Sejarah Bendungan Pice</h1>
+
+<div class="container1">
+        <div class="pice2">
+            <img src="img/sejarah.png" alt="pice1">
+            <p>Foto lama<i> - Bendungan Pice</i></p>
+        </div>
+        <div class="ygpenting">
+            
+            <p class="indented-text"> 
+Sejak tahun 1722, kongsi dagang VOC mengadakan perjanjian dengan Sultan Ratu Anum Kamarduin dari Palembang untuk membeli timah secara monopoli. Maka sejak saat itu, VOC memulai kecurangan-kecurangan dan pelanggaran janji yang menimbulkan ketegangan dan sikap permusuhan. Pada tahun 1759, Residen Palembang De Herre melakukan penelitian, ia kemudian menyimpulkan bahwa tidak ada biji timah Disana. Timah Kopong adalah pasir yang mirip pasir timah, tapi tidak mengandung timah. Konon pengkopongan pasir timah dilakukan oleh sejenis dukun kampong yang ada di Belitung. Pada tanggal 21 Oktober 1981, Syarif Mohamad datang dari Palembang untuk membuka jalan bagi masuknya Belanda ke Belitung dan mengibarkan pertama kali bendera Belanda di Tanjung Sinba. Ia berusaha menguasai Depati K.A Rahad yang masih muda, akan tetapi usaha tersebut tidak memperoleh hasil.
+</p>
+
+        </div>
+    </div>
+
+    <div class="container2">
+        <div class="penjelasan2">
+            <p class="indented-text">Pada tahun 1822, ditempatkannya bala tentara dibawah pimpinan seorang Kapten Bangsa Belgia yang bernama J.P. De La Motte. Tahun 1823, J.P De La Motte dipanggil kembali dan digantikan oleh J.R Bierschel sebagai asisten Residen dan Kapten Kuehn yang sebagai pimpinan tentara karena usahanya menemukan timah gagal. Karena J.R Bierschel memerintah dengan bijaksana, maka menarik perhatian dan pengertian baik dari masyarakat. Ia bahkan berulang kali mendesak pemerintah Belanda hak Depati K.A Rahad itu diakui, tapi itu sia-sia. Sementara itu Bierschel tidak ketinggalan mencari timah dan akhirnya menemukannya, tetapi keinginannya ditolak. Siak akomodati dari Asisten Residen J. W Bierschel tergadap K.A Rahad membuat Syarif Mohamad merasa tidak senang karena seolah-olah mengacuhkannya yang semula diberi kepercayaan untuk menguasai Belitung. Ia kemudian pulang dan melaporkan segala tindak- tanduk J. W Bierschel, yang akhirnya J. W Bierschel ditarik pulang kembali. Sebenarnya, bukan karena alasan itu J. W Bierschel ditarik pulang, melainkan karena untuk penghematan biaya dan hak-hak penggalian timah belum diakui Belanda.</p>
+        </div>
+        <div class="image2">
+            <img src="img/sejarah2.png" alt="Gambar2">
+            <p>Foto lama<i> - Bendungan Pice</i></p>
+        </div>
+    </div>
+
+    <div class="container3">
+        <div class="image3">
+            <img src="img/sejarah3.jpg" alt="Gambar3">
+            <p>Foto<i> - Bendungan Pice</i></p>
+        </div>
+        <div class="penjelasan3">
+            <p class="indented-text"> Pada tahun 1838, Belanda tim ekspedisi yang dipimpin oleh P.C Riedel, guna menyelidiki dan mencari tahu tentang sikap Depati yang tidak dapat ditundukkan oleh sekian banyak orang pemimpin baik Belanda. Akan tetapi, P.C Riedel juga mendengarkan anjuran Residen Bangka De Hasee dan mantan Residen Belitung, yaitu J.W Bierschel agar mengakui hak-hak K.A Rahad sebagai Bupati Belitung. Tahun 1850, Dr. J.H Croockewit diutus pemerintah Belanda untuk melakukan penyelidikan mengenai kemungkinan biji timah di Belitung. Akan tetapi, ia mengalami kegagalan karena tidak mendapat dukungan rakyat dan K.A Rahad.
+Setelah melalui berbagai usaha, akhirnya ia bersama dengan beberapa orang mendapat informasi dari rakyat mengenai keberadaan biji timah. Setelah berhasil menemukan lokasinya, mereka mendatangkan pekerja timah dari cina dan pekerja timah dari cina itu sering disebut sebagai Cina Kuncit, sebab mereka mempunyai rambut panjang dibagian belakang yang dikuncit. Perusahaan penambangan timah awal mulanya bernama Billiton Maatschapy yang kemudian berubah nama menjadi NV GMB, selanjutnya kedatangan kapal keruk yang bakal dipakai untuk mengeruk tanah di lokasi dan memulai pencarian timah.
+Beberapa lokasi pengerukan timah dan salah satunya adalah di Sungai Lenggang yang menghasilkan banyak timah, sehingga pencarian dan penggalian timah semangkin dalam dan air semangkin banyak yang akhirnya menyebabkan pengoperasian kapi keruk terhenti. Hal ini lah yang membuat pihak Belanda berpikir keras bagaimana agar air tersebut dapat dikontrol sehingga pengoperasian dapat beijaan kembali. Kemudian muncullah ide untuk membangun bendungan yang berfungsi sebagai pengontrol air tersebut. Maka dibangunkanlah bendungan yang kemudian bernama Bendungan Pice yang sering dipanggil dengan sebutan Pice. Nama Pice sendiri berasal dan sebutan orang Cina yang menyebutkan nam Phice yang artinya pintu air, sehingga masyarakat Belitung menamainya dengan sebutan Pice, tetapi ada pula yang menyebutkan nama Pice itu berasal dari nama Sir Vance yang merupakan seorang arsitek insinyur Belanda yang membangun bendungan tersebut. Akan tetapi pada tahun 1953, Pemerintah NI mengambil alih perusahaan timah dan perusahaan timah beralih nama menjadi PN Timah. Tahun 1958, PN Timah membangun sebuah bendungan baru yang terarah ke laut dan ukurannya tidak begitu besar tetapi membantu Pice dalam mengatur air. Setelah beberapa lama beroperasi, muncullah pendapat bahwa pertambangan timah dibelitung sudah tidak produktif dan mereka berpendapat jika diteruskan maka akan mengalami kerugian besar karena biaya operasionalnya lebih besar dibandingkan dengan hasil yang mereka peroleh. Maka tahun 1991, KK IX Meranteh keluar dari Sungai Lenggang menuju Pulau Bangka.
+</p>
+        </div>
+    </div>
+
+ 
+
+<!-- 
+    footer -->
+    <footer class="footer-distributed">
+
+<div class="footer-left">
+<img src="img/sipekaaja.png">
+    <h3>Sipe<span>Ka</span></h3>
+
+    <p class="footer-links">
+        <a href="#">Home</a>
+        |
+        <a href="#">Blog</a>
+        |
+        <a href="#">About</a>
+        |
+        <a href="#">Contact</a>
+    </p>
+
+    <p class="footer-company-name"> Sipeka app © 2023 </p>
+</div>
+
+<div class="footer-center">
+    <div>
+        <i class="fa fa-map-marker"></i>
+          <p><span>25R8+733,
+          Kec. Gantung, Kabupaten Belitung Timur,</span>
+          Kepulauan Bangka Belitung</p>
+    </div>
+
+    <div>
+        <i class="fa fa-phone"></i>
+        
+    </div>
+    <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="">teampillarpice@gmail.com</a></p>
+    </div>
+</div>
+<div class="footer-right">
+    <p class="footer-company-about">
+        <span>Tentang Kami</span>
+        Ini adalah situs web resmi untuk informasi tentang Proyek KBKM Kemah Budaya 2023 di Desa Lenggang, Kelompok Bendungan Pice.</p>
+    <div class="footer-icons">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-youtube"></i></a>
+        <br>
+        <img src="img/kemendikbud.png" alt="">
+        <img src="img/kbkm.png" alt="">
+       
+    </div>
+</div>
+</footer>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+</body>
+</html>
---
+++ sopuser.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<link rel="icon" href="img/sipekaaja.png" type="image/png">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SiPeka | Sejarah</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:400,700&display=swap">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.22.0/font/bootstrap-icons.css">
+
+    
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="navbar" id="navbar">
+    <div class="logo">
+        <a href="#">
+            <img src="img/sipekaaja.png" alt="Logo"> 
+            Si<span>Peka</span>
+        </a>
+    </div>
+    <div class="menu">
+    <a href="index.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-speedometer2" viewBox="0 0 16 16">
+  <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
+  <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
+</svg> Dashboard</a>
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-history" viewBox="0 0 16 16">
+  <path d="M8.515 1.019A7 7 0 0 0 8 1V0a8 8 0 0 1 .589.022l-.074.997zm2.004.45a7.003 7.003 0 0 0-.985-.299l.219-.976c.383.086.76.2 1.126.342l-.36.933zm1.37.71a7.01 7.01 0 0 0-.439-.27l.493-.87a8.025 8.025 0 0 1 .979.654l-.615.789a6.996 6.996 0 0 0-.418-.302zm1.834 1.79a6.99 6.99 0 0 0-.653-.796l.724-.69c.27.285.52.59.747.91l-.818.576zm.744 1.352a7.08 7.08 0 0 0-.214-.468l.893-.45a7.976 7.976 0 0 1 .45 1.088l-.95.313a7.023 7.023 0 0 0-.179-.483zm.53 2.507a6.991 6.991 0 0 0-.1-1.025l.985-.17c.067.386.106.778.116 1.17l-1 .025zm-.131 1.538c.033-.17.06-.339.081-.51l.993.123a7.957 7.957 0 0 1-.23 1.155l-.964-.267c.046-.165.086-.332.12-.501zm-.952 2.379c.184-.29.346-.594.486-.908l.914.405c-.16.36-.345.706-.555 1.038l-.845-.535zm-.964 1.205c.122-.122.239-.248.35-.378l.758.653a8.073 8.073 0 0 1-.401.432l-.707-.707z"/>
+  <path d="M8 1a7 7 0 1 0 4.95 11.95l.707.707A8.001 8.001 0 1 1 8 0v1z"/>
+  <path d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"/>
+</svg> Sejarah</a>
+        <a href="kejadian.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-collection" viewBox="0 0 16 16">
+  <path d="M2.5 3.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm2-2a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zm1.5.5A.5.5 0 0 1 1 13V6a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13z"/>
+</svg> Kejadian</a>
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book-fill" viewBox="0 0 16 16">
+  <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+</svg> SOP</a>
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+  <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
+</svg> Kontak</a>
+    </div>
+</div>
+
+<div style="margin-top: 60px;">
+   
+</div>
+
+    
+<!-- header -->
+
+
+
+<div id="apanih">
+    <br>
+<p>Sipeka | SOP > Standard Operating Procedure Saat Terjadi Banjir
+
+ 
+
+</p>
+      <br>
+    </div>
+
+    <br>
+ 
+    <div class="himbauan">
+        <h1>Himbauan Kepada Masyarakat</h1>
+        <br>
+        <p><span>1.</span> Masyarakat memiliki tanggung jawab utama untuk memastikan keselamatan diri sendiri dan orang lain dalam situasi darurat, terutama jika ketinggian air bendungan meningkat secara tiba-tiba atau ada ancaman banjir.</p>
+
+<p>2. Pelaksanaan Evakuasi: Masyarakat harus mematuhi instruksi evakuasi yang dikeluarkan oleh pihak berwenang atau administrator. Masyarakat harus mengikuti rute evakuasi yang telah ditentukan dan membantu orang lain yang membutuhkan pertolongan.</p>
+
+<p>3. Pemberitahuan Darurat: Jika Masyarakat melihat atau merasa bahwa ada situasi darurat yang mengancam keselamatan, Masyarakat harus segera memberi tahu pihak berwenang setempat atau pihak yang berwenang.</p>
+
+<p>4. Kesiapsiagaan Pribadi: Masyarakat harus siap dengan peralatan keselamatan pribadi dan tahu cara menggunakannya, seperti pelampung atau peralatan penyelamatan.</p>
+    </div>
+    <div class="jagoan">
+    <br>
+    <h3>4 Jenis Himbauan</h3>
+    <div id="status-container">
+<div class="status status-aman">
+        <p><a href="sopuser.php">Aman</a></p>
+    </div>
+    <div class="status status-waspada">
+        <p><a href="sopuser.php">WaspadaSiaga 3</a></p>
+    </div>
+    <div class="status status-siaga">
+        <p><a href="sopuser.php">Siaga 2</a></p>
+    </div>
+    <div class="status status-awas">
+        <p><a href="sopuser.php">Awas Siaga 1</a></p>
+    </div>
+</div>
+    <br>
+    <br>
+
+
+<!-- 
+    footer -->
+    <footer class="footer-distributed">
+
+<div class="footer-left">
+<img src="img/sipekaaja.png">
+    <h3>Sipe<span>Ka</span></h3>
+
+    <p class="footer-links">
+        <a href="#">Home</a>
+        |
+        <a href="#">Blog</a>
+        |
+        <a href="#">About</a>
+        |
+        <a href="#">Contact</a>
+    </p>
+
+    <p class="footer-company-name"> Sipeka app © 2023 </p>
+</div>
+
+<div class="footer-center">
+    <div>
+        <i class="fa fa-map-marker"></i>
+          <p><span>25R8+733,
+          Kec. Gantung, Kabupaten Belitung Timur,</span>
+          Kepulauan Bangka Belitung</p>
+    </div>
+
+    <div>
+        <i class="fa fa-phone"></i>
+        
+    </div>
+    <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="">teampillarpice@gmail.com</a></p>
+    </div>
+</div>
+<div class="footer-right">
+    <p class="footer-company-about">
+        <span>Tentang Kami</span>
+        Situs Ini Dibangun sebagai bahan pengabdian dan riset terrbuka untuk seluruh lapisan masyarakat.</p>
+    <div class="footer-icons">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-youtube"></i></a>
+        <br>
+        <img src="img/kemendikbud.png" alt="">
+        <img src="img/kbkm.png" alt="">
+        <img src="img/pupr.png" alt="">
+        <img src="img/bnpb.png" alt="">
+    </div>
+</div>
+</footer>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+</body>
+</html>
